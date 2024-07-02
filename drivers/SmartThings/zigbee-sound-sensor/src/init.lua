424a5566407d1b23f46df243d3e73dd67b14ac65
wkhenon <dev.kwalker@gmail.com>
2024-07-02 10:46:16 -0400

    zigbee-sound-sensor: updated with min, max, and measured temp values

--- drivers/SmartThings/zigbee-sound-sensor/src/init.lua
+++ drivers/SmartThings/zigbee-sound-sensor/src/init.lua
@@ -21,7 +21,9 @@ local zcl_clusters = require "st.zigbee.zcl.clusters"
 local PollControl = zcl_clusters.PollControl
 local IASZone = zcl_clusters.IASZone
 local PowerConfiguration = zcl_clusters.PowerConfiguration
+local TemperatureMeasurement = zcl_clusters.TemperatureMeasurement
 local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+local temperature_defaults = require "st.zigbee.defaults.temperatureMeasurement_defaults"
 
 local CHECK_IN_INTERVAL = 6480
 local LONG_POLL_INTERVAL = 1200
@@ -52,6 +54,59 @@ local ias_zone_status_change_handler = function(driver, device, zb_rx)
   generate_event_from_zone_status(driver, device, zb_rx.body.zcl_body.zone_status, zb_rx)
 end
 
+--- Default handler for Temperature measured value on the Temperature measurement cluster
+---
+--- This converts the Int16 value of the temp measured attribute to TemperatureMeasurement.temperature this will convert
+--- to fahrenheit or celsius based on the device data
+---
+--- @param driver Driver The current driver running containing necessary context for execution
+--- @param device ZigbeeDevice The device this message was received from containing identifying information
+--- @param value Int16 the value of the measured temperature
+--- @param zb_rx containing the full message this report came in
+
+local function temperature_measurement_attr_handler(driver, device, value, zb_rx)
+  local raw_temp = value.value
+  local celc_temp = raw_temp / 100.0
+  local temp_scale = "C"
+  -- All events from drivers should be in celsius and without offset manipulation
+  device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.temperatureMeasurement.temperature({value = celc_temp, unit = temp_scale }))
+end
+
+--- Default handler for Temperature min and max measured value on the Temperature measurement cluster
+---
+--- This starts initially by performing the same conversion in the temperature_measurement_attr_handler function.
+--- It then sets the field of whichever measured value is defined by the @param and checks if the fields
+--- correctly compare
+---
+--- @param minOrMax string the string that determines which attribute to set
+--- @param driver Driver The current driver running containing necessary context for execution
+--- @param device ZigbeeDevice The device this message was received from containing identifying information
+--- @param value Int16 the value of the measured temperature
+--- @param zb_rx containing the full message this report came in
+
+local temperature_measurement_min_max_attr_handler = function(minOrMax)
+  return function(driver, device, value, zb_rx)
+    local raw_temp = value.value
+    local celc_temp = raw_temp / 100.0
+    local temp_scale = "C" 
+
+    device:set_field(string.format("%s", minOrMax), celc_temp)
+
+    local min = device:get_field(temperature_defaults.MIN_TEMP)
+    local max = device:get_field(temperature_defaults.MAX_TEMP)
+
+    if min ~= nil and max ~= nil then
+      if min < max then
+        device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.temperatureMeasurement.temperatureRange({ value = { minimum = min, maximum = max }, unit = temp_scale }))
+        device:set_field(temperature_defaults.MIN_TEMP, nil)
+        device:set_field(temperature_defaults.MAX_TEMP, nil)
+      else
+        device.log.warn_with({hub_logs = true}, string.format("Device reported a min temperature %d that is not lower than the reported max temperature %d", min, max))
+      end
+    end
+  end
+end
+
 local do_configure = function(self, device)
   device_management.write_ias_cie_address(device, self.environment_info.hub_zigbee_eui)
   device:send(IASZone.server.commands.ZoneEnrollResponse(device, 0x00, 0x00)) -- ZoneEnroll Response should be called first in case of this device.
@@ -79,6 +134,11 @@ local zigbee_sound_sensor_driver_template = {
     attr = {
       [IASZone.ID] = {
         [IASZone.attributes.ZoneStatus.ID] = ias_zone_status_attr_handler
+      },
+      [TemperatureMeasurement.ID] = {
+        [TemperatureMeasurement.attributes.MeasuredValue.ID] = temperature_measurement_attr_handler,
+        [TemperatureMeasurement.attributes.MinMeasuredValue.ID] = temperature_measurement_min_max_attr_handler(temperature_defaults.MIN_TEMP),
+        [TemperatureMeasurement.attributes.MaxMeasuredValue.ID] = temperature_measurement_min_max_attr_handler(temperature_defaults.MAX_TEMP),
       }
     },
     cluster = {
